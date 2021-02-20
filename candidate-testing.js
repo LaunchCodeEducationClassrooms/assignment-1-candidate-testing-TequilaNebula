526cea959eb5c5f2f10b5b7aaa9db67853401dba
Amber Swope <abemily93@gmail.com>
2021-02-20 00:57:55 +0000

    This should be that last debug for that green boi

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,7 +12,7 @@ let candidateAnswer = "";
 let questions = [
   "Who was the first American woman in space? ", 
   "True or False: 5000 meters = 5 kilometers. ", 
-  "(5+3)/2*10=? ", 
+  "(5 + 3)/2 * 10 = ? ", 
   "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
   "What is the minimum crew size for the ISS? "
   ];
