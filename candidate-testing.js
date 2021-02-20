1f0a7b09ae64189a3f83d38d0d7548a6a9c0763d
Amber Swope <abemily93@gmail.com>
2021-02-20 00:46:16 +0000

    Okay, this is the last attempt. GREEN CHECKMARK OF WONDER PLEASE SHOW UP

--- candidate-testing.js
+++ candidate-testing.js
@@ -13,7 +13,7 @@ let questions = [
   "Who was the first American woman in space? ", 
   "True or False: 5000 meters = 5 kilometers. ", 
   "(5+3)/2*10=? ", 
-  "Given the array 98, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
+  "Given the array [98, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
   "What is the minimum crew size for the ISS? "
   ];
 let correctAnswers = [
